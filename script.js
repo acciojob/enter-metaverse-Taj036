16795dfa5c282615fca932da97c31606425f9052
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-18 22:45:10 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+//your JS code here. If requiIed.
+function entermetaverse(){
+	let ele = document.getElementById('status');
+	ele.innerHTML = "<h1> Entered Metaverse <\h1>" ;
+}
